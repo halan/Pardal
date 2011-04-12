0c8296b4dff1b18c7f51194465379ed0c1fecb8a
Mario Felipe Rinaldi <mrinaldi@sp.r7.com>
2011-04-12 19:30:51 -0300

    cutting out the fats! JSLint almost pass...

--- chrome/content/javascripts/main.js
+++ chrome/content/javascripts/main.js
@@ -1,33 +1,26 @@
 /*--- spliting columns --*/
 
-var split_columns = function(width)
-{
+var split_columns = function (width) {
   var min_column = 300;
-  var columns = Math.min(parseInt(width/200), $('tabpanel').size());
+  var columns = Math.min(parseInt(width / 200), $('tabpanel').size());
   var current_columns_size = $('#timeline browser').size();
 
-  if(current_columns_size < columns)
-  {
-    for(column=1; column < columns; column++)
-    {
+  if (current_columns_size < columns) {
+    for (column=1; column < columns; column++) {
       var browser = $('browser', $('tabpanel').get(column)).get(0);
       $('tabpanel').get(0).appendChild(browser);
       $($('tab').get(column)).hide();
     }
 
-    $('tab[selected=true]:not(:visible)').each(function()
-    {
+    $('tab[selected=true]:not(:visible)').each(function () {
       $('tabpanels').attr('selectedIndex', 0);
       $('tab:first').attr('selected', true);
     });
   }
-  else if( current_columns_size > columns)
-  {
+  else if ( current_columns_size > columns) {
     var join_columns_size = current_columns_size - columns; 
-    for(column=0; column < current_columns_size; column++)
-    {
-      if(!column < columns)
-      {
+    for (column=0; column < current_columns_size; column++) {
+      if (!column < columns) {
         var browser = $('#timeline browser').get(1);
         $('tabpanel').get(column).appendChild(browser);
         $($('tab').get(column)).show();
@@ -36,15 +29,12 @@ var split_columns = function(width)
   }
 };
 
-
-$(window).resize(function()
-{
+$(window).resize(function () {
   split_columns(window.outerWidth);
 });
 
-
 /*--- counter ---*/
-var counter = function(input){
+var counter = function (input) {
 
   var $$ = $(input),
       container = $$.parents('#text-enter'),
@@ -52,8 +42,8 @@ var counter = function(input){
       limit = $$.attr('limit'),
       n = input.value.length;
 
-  if (n > limit) container.addClass('counter-overflow');
-  else container.removeClass('counter-overflow');
+  if (n > limit) {container.addClass('counter-overflow');}
+  else {container.removeClass('counter-overflow');}
 
   counter.text( parseInt(limit) - parseInt(n) );
 };
