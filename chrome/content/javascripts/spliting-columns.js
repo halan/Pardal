a7d6616352dd666ba4d97c70347d81f8d8cec290
Mario Felipe Rinaldi <mrinaldi@sp.r7.com>
2011-04-15 14:55:19 -0300

    just moving vars to begin of function.

--- chrome/content/javascripts/spliting-columns.js
+++ chrome/content/javascripts/spliting-columns.js
@@ -1,13 +1,14 @@
 /*--- spliting columns --*/
 
 var split_columns = function (width) {
-  var min_column = 300;
-  var columns = Math.min(parseInt(width / 200), $('tabpanel').size());
-  var current_columns_size = $('#timeline browser').size();
+  var min_column, columns, column, current_columns_size, browser;
+  min_column = 300;
+  columns = Math.min(parseInt(width / 200), $('tabpanel').size());
+  current_columns_size = $('#timeline browser').size();
 
   if (current_columns_size < columns) {
     for (column=1; column < columns; column++) {
-      var browser = $('browser', $('tabpanel').get(column)).get(0);
+      browser = $('browser', $('tabpanel').get(column)).get(0);
       $('tabpanel').get(0).appendChild(browser);
       $($('tab').get(column)).hide();
     }
@@ -21,7 +22,7 @@ var split_columns = function (width) {
     var join_columns_size = current_columns_size - columns; 
     for (column=0; column < current_columns_size; column++) {
       if (!column < columns) {
-        var browser = $('#timeline browser').get(1);
+        browser = $('#timeline browser').get(1);
         $('tabpanel').get(column).appendChild(browser);
         $($('tab').get(column)).show();
       }
